d3998d15cc2abdd517d0ea608bbe685391c771ad
Michael Herman <hermanmu@gmail.com>
2021-08-22 09:32:54 -0500

    init

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200, blank=False)
+    price = models.CharField(max_length=20, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
